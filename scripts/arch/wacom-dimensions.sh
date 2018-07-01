adc0520782f10b47c4d24101f930738f12f381d4
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2018-07-01 11:17:03 +0100

    Updated Wacom screen dim script

--- scripts/arch/wacom-dimensions.sh
+++ scripts/arch/wacom-dimensions.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-xsetwacom --set "Wacom Bamboo 2FG 6x8 Pen Stylus" MapToOutput 2560x1440+1920+0
+xsetwacom --set "Wacom Bamboo 2FG 6x8 Pen stylus" MapToOutput 2560x1440+1920+0
+xsetwacom --set "Wacom Bamboo 2FG 6x8 Pen eraser" MapToOutput 2560x1440+1920+0
+xsetwacom --set "Wacom Bamboo 2FG 6x8 Finger touch" touch off

1539221596df9994ca0c251da0eacdf03ea396a0
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2019-10-01 17:37:30 +0100

    Modified Wacom scripts to cater for swapped screens

---
+++ scripts/arch/wacom-dimensions-left.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+xsetwacom --set "Wacom Bamboo 2FG 6x8 Pen stylus" MapToOutput 2560x1440+0+0
+xsetwacom --set "Wacom Bamboo 2FG 6x8 Pen eraser" MapToOutput 2560x1440+0+0
+xsetwacom --set "Wacom Bamboo 2FG 6x8 Finger touch" touch off

37c40c954a424108f40e9480a61d6ea6b9697c44
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2021-05-17 19:20:24 +0100

    Added conky for PC as well as laptop

---
+++ conky/minimalis/Readme.md
@@ -0,0 +1,3 @@
+ConkySymbols.ttf will need to be installed as a font on the machine in order for the correct symbols to be shown.
+
+Additionally, to get temperature readings, the `lm-sensors` package must be installed.
---
+++ conky/minimalis/start_pc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# These things don't die easily, and for some reason KDE refuses to
+# kill them off properly even if this command is run on logout, so
+# I'm putting it here to kill off any residual processes before
+# starting new ones.
+# killall conky
+
+# sleep 10
+conky -c "$SCRIPT_DIR/conky_right_pc.conf" #&

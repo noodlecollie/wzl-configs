46ea2ab256e56efcc9f1fe7739fb09f839c2f602
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-05-21 18:27:26 +0100

    yt-dlp tweaks

--- yt-dlp/download.bat
+++ yt-dlp/download.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 320K --output "%%(title)s.%%(ext)s" %1
+yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 320K --output "%%(title)s.%%(ext)s" "%1"

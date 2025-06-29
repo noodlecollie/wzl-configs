26c1aa735b410a63e151368cb04b3624dc6eab58
Vesper <3692034+noodlecollie@users.noreply.github.com>
2025-06-29 20:47:55 +0100

    Fixed env file name in mount-remote-fs

--- backups/mount-remote-fs.sh
+++ backups/mount-remote-fs.sh
@@ -2,11 +2,11 @@
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-if [[ -f "$SCRIPT_DIR/credentials_env" ]];
+if [[ -f "$SCRIPT_DIR/backblaze.env" ]];
 then
-	source "$SCRIPT_DIR/credentials_env"
+	source "$SCRIPT_DIR/backblaze.env"
 	mkdir -p "$SCRIPT_DIR/mount"
 	restic -r "b2:wzl-vault:main-desktop-docs/" mount "$SCRIPT_DIR/mount"
 else
-	echo "$SCRIPT_DIR/credentials_env does not exist"
+	echo "$SCRIPT_DIR/backblaze.env does not exist"
 fi
